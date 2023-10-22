c6e4a4f6e9cc949994c4d91128f47234d6f1b895
am999 <am9999072080@gmail.com>
2023-10-23 02:26:48 +0300

    Commit_2

--- src/main/java/com/gridnine/testing/FlightFilter.java
+++ src/main/java/com/gridnine/testing/FlightFilter.java
@@ -2,19 +2,16 @@ package com.gridnine.testing;
 
 public interface FlightFilter {
     /**
-     * Показывает в ожидании вылета в настоящее время
      * Currently awaiting departure
      */
     boolean waitingForDepartureFlight();
 
     /**
-     * Исключает рейсы, где прибытие раньше, чем дата вылета
      * Excludes flights where arrival is earlier than departure date
      */
     boolean excludeArrivalBeforeDepartureDate();
 
     /**
-     * Рейсы, где общее время, проведенное на земле, превышает указанное время.
      * Total time on the ground exceeds the specified time.
      */
     boolean longTimeOnEarth();
